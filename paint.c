eb505094368678beb199008e62d11f4d2737f171
BiggyLNX <aum.patel1107@gmail.com>
2024-04-19 10:09:04 -0700

    Version 2: Read Static Touch Data

--- paint.c
+++ paint.c
@@ -1,8 +1,4 @@
-/*
-Name: Aum Patel
-Description: This Code implements basic integration for TSC2007 User space
-Version 1.0
-*/
+//v2
 #include <stdio.h>
 #include <stdlib.h>
 #include <fcntl.h>
@@ -17,7 +13,8 @@ Version 1.0
 typedef struct {
     int16_t x;
     int16_t y;
-    int16_t z;
+    int16_t z1;
+    int16_t z2;
 } TS_Point;
 
 int main() {
@@ -33,16 +30,19 @@ int main() {
     }
 
     while (1) {
-        uint8_t buf[2];
+        uint8_t buf[8];
         if (read(i2c_fd, buf, sizeof(buf)) != sizeof(buf)) {
-            perror("Error reading       from I2C bus");
+            perror("Error reading from I2C bus");
             return EXIT_FAILURE;
         }
 
         int16_t x = (buf[0] << 8) | buf[1];
+        int16_t y = (buf[2] << 8) | buf[3];
+        int16_t z1 = (buf[4] << 8) | buf[5];
+        int16_t z2 = (buf[6] << 8) | buf[7];
 
-        if (x != 0) { // Check if touch coordinates have changed
-            printf("X: %d\n", x);
+        if (z1 > 0 && z2 > 0) { // Check if touch detected
+            printf("Touch point: (X: %d, Y: %d, Z1: %d, Z2: %d)\n", x, y, z1, z2);
         }
 
         usleep(10000); // Short delay before next read
@@ -51,4 +51,3 @@ int main() {
     close(i2c_fd);
     return EXIT_SUCCESS;
 }
-
